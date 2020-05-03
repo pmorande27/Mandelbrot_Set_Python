72f225117cc41b811482050f2a0b1db5861638a0
pmorande27 <pmorande27@gmail.com>
2020-05-03 14:27:28 +0200

    minor refactor changes

--- MainManldbrot.py
+++ MainManldbrot.py
@@ -7,9 +7,12 @@ Created on Thu Feb 20 11:21:51 2020
 import numpy as np
 import matplotlib.pyplot as plt
 from MandelbrotSet import mandlebrot
+
+
 def main():
-    mymandlebrot =mandlebrot(10000)
-    plt.show(mymandlebrot.printMandlebrot())
-    
-    
+    mymandlebrot = mandlebrot(1000)
+    a = (mymandlebrot.printMandlebrot())
+    plt.show()
+
+
 main()
--- MandelbrotSet.py
+++ MandelbrotSet.py
@@ -6,28 +6,29 @@ Created on Thu Feb 20 10:52:38 2020
 """
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 class mandlebrot(object):
     def __init__(self, resolution):
         self.resolution = resolution
-    def isMandlebrot(self,realN, complexN):
-        z0 = complex(0,0) 
-        C = complex (realN,complexN)
+
+    def isMandlebrot(self, realN, complexN):
+        z0 = complex(0, 0)
+        C = complex(realN, complexN)
         result = 0
         for i in range(255):
-            if (np.abs(z0)<2):
-                z0 =z0*z0 + C
+            if (np.abs(z0) < 2):
+                z0 = z0 * z0 + C
             else:
                 result = i
                 break
         return (result)
-    def printMandlebrot(self,):
-        lineSpaceX = np.linspace(-2.025,0.6,self.resolution)
-        lineSpaceY = np.linspace(-1.125,1.125,self.resolution)
-        resultArray = np.zeros((self.resolution,self.resolution))
+
+    def printMandlebrot(self, ):
+        lineSpaceX = np.linspace(-2.025, 0.6, self.resolution)
+        lineSpaceY = np.linspace(-1.125, 1.125, self.resolution)
+        resultArray = np.zeros((self.resolution, self.resolution))
         for i in range(self.resolution):
             for j in range(self.resolution):
-                resultArray[i][j] = self.isMandlebrot(lineSpaceX[i],lineSpaceY[j])
-        return (plt.imshow(resultArray.T,extent = [-2.025,0.6,-1.125,1.125]))
-       
-            
-                        
+                resultArray[i][j] = self.isMandlebrot(lineSpaceX[i], lineSpaceY[j])
+        return plt.imshow(resultArray.T)
