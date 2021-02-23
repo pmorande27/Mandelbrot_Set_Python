aa36ba48907da0337593cf000dc60db1c2ff1864
pmorande27 <pmorande27@gmail.com>
2021-02-23 16:24:49 +0100

    Update solution

--- MainManldbrot.py
+++ MainManldbrot.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 20 11:21:51 2020
-
+Main file for Mandelbrot set
 @author: Pablo Morandé
 """
+from MandelbrotSet import Mandelbrot
 import numpy as np
-import matplotlib.pyplot as plt
-from MandelbrotSet import mandlebrot
-
-
-def main():
-    mymandlebrot = mandlebrot(1000)
-    a = (mymandlebrot.printMandlebrot())
-    plt.show()
-
+def main ():
+    mySet = Mandelbrot(500, 500)
+    mySet.printMandlebrot()
 
 main()
+
+
--- MandelbrotSet.py
+++ MandelbrotSet.py
@@ -1,34 +1,51 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 20 10:52:38 2020
-
+File for creating the class Mandelbrot
 @author: Pablo Morandé
 """
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-class mandlebrot(object):
-    def __init__(self, resolution):
-        self.resolution = resolution
+class Mandelbrot(object):
+    def __init__(self,x_dimension,y_dimension):
+        """
+        Constructor of the Class to build the mandelbrot, 
+        it will take as parameters the number of points to be analysed
+        """
+        self.x_dimension = x_dimension
+        self.y_dimension = y_dimension
 
-    def isMandlebrot(self, realN, complexN):
-        z0 = complex(0, 0)
-        C = complex(realN, complexN)
-        result = 0
-        for i in range(255):
-            if (np.abs(z0) < 2):
-                z0 = z0 * z0 + C
-            else:
-                result = i
-                break
-        return (result)
+    def isMandelbrot(self, c):
+         iterations = 0
+         z = 0
+         magnitude = 0
+         while magnitude <= 2 and iterations < 255:
+             nextZ = z*z + c
+             magnitude = abs(nextZ)
+             z = nextZ
+             iterations += 1
+         return iterations
 
     def printMandlebrot(self, ):
-        lineSpaceX = np.linspace(-2.025, 0.6, self.resolution)
-        lineSpaceY = np.linspace(-1.125, 1.125, self.resolution)
-        resultArray = np.zeros((self.resolution, self.resolution))
-        for i in range(self.resolution):
-            for j in range(self.resolution):
-                resultArray[i][j] = self.isMandlebrot(lineSpaceX[i], lineSpaceY[j])
-        return plt.imshow(resultArray.T)
+        """
+        Method used to construct and display the Mandelbrot.
+        """
+        "Create Space of imaginary and real terms"
+        lineSpaceX = np.linspace(-2.025, 0.6, self.x_dimension)
+        lineSpaceY = np.linspace(-1.125, 1.125, self.y_dimension)
+        XX, YY = np.meshgrid(lineSpaceX,lineSpaceY )
+        "Initialize result array"
+        resultArray = np.zeros((self.x_dimension, self.y_dimension))
+        "Calculate Mandelbrot set"
+        for i in range( self.x_dimension):
+            for j in range( self.y_dimension):
+                resultArray[j][i] = self.isMandelbrot(complex(XX[j][i],YY[j][i]))
+        "Plot"
+        plt.xlabel('x axis')
+        plt.ylabel('y axis')
+        plt.axis('on')
+        plt.imshow(resultArray,extent=[-2.025, 0.6,-1.125, 1.125])
+        plt.colorbar()
+        plt.title('MandelBrot Set')
+        plt.show()
